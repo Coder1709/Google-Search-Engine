8349a764975b814c17eb782c6f14f344be8bfbb7
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-11-17 15:19:17 +0530

    Added Header

--- src/app/component/HomeHeader.jsx
+++ src/app/component/HomeHeader.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { TbGridDots } from "react-icons/tb";
 
 export default function HomeHeader() {
   return (
-    <div>
-     HomeHeader 
-    </div>
-  )
+    <header className="">
+      <div className="flex justify-end p-5 text-sm space-x-4 items-center">
+        <Link
+          className="hover:underline"
+          href="https://mail.google.com"
+          target="_blank"
+        >
+          Gmail
+        </Link>
+
+        <Link
+          className="hover:underline"
+          href="https://image.google.com"
+          target="_blank"
+        >
+          Image
+        </Link>
+        <TbGridDots className="bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2" />
+        <button className="bg-blue-600 text-white text-2xl rounded-md px-6 py-2 font-medium hover:brightness-105 hover:shadow-md transition-shadow"> Sign In</button>
+      </div>
+    </header>
+  );
 }
