8c3548920244fbdb0bdfb1444ff2a171fa402517
Arpit Pathak <110691809+Coder1709@users.noreply.github.com>
2023-11-17 16:43:12 +0530

    final header changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Google ',
+  title: 'Google',
   description: 'Created By Arpit Pathak',
 }
 
